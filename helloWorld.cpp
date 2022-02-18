5fe1d2757557104c6255c6058fc1e14dc384fcc7
Jackson Naufal <Jackson.naufal@mths.ca>
2022-02-18 12:58:52 +0000

    HelloWorldprogram

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Jackson Naufal All rights reserved
+//
+// Created By Jackson Naufal
+// Created On Feb 2022
+// This is a "Hello, World!" program with proper style"
+
+
+
+#include <iostream>
+
+int main()
+{std::cout << "Hello, World!" << std::endl;}
